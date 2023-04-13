219143dc44cac286cb7d4da4e57379845171e283
Lina García-Cabrera <lina@ujaen.es>
2023-04-13 12:36:25 +0200

    Update 100-ciclos-for-anidados.js

--- Temas 100-129/100-ciclos-for-anidados.js
+++ Temas 100-129/100-ciclos-for-anidados.js
@@ -10,7 +10,7 @@ var miArreglo = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 for (var i = 0; i < arreglo.length; i++) {
   var arregloAnidado = miArreglo[i];
   // Por cada elemento del arreglo anidado
-  for (var j = 0; j < fila.length; j++) {
+  for (var j = 0; j < arregloAnidado.length; j++) {
     console.log(arregloAnidado[j]);
   }
 }
